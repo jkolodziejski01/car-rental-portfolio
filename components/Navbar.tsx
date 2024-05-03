adf0ccbc6399b60fbab3be8e6d240b43814a1e73
jkolodziejski01 <jkolodziejski01@gmail.com>
2024-05-03 02:41:07 +0200

    started template added Navbar component.

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='mt-5 flex items-center justify-around'>
+        <h1>CARENTAL</h1>
+        <div className="flex gap-5">
+            <h2>Home</h2>
+            <h2>Contact Us</h2>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
