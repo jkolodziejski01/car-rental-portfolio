ce0468c947ced199259553802bc8f8a3b453caf9
jkolodziejski01 <jkolodziejski01@gmail.com>
2024-05-05 02:24:36 +0200

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <div className="p-5 sm:px-10 md:px=20">
       <HeroSection />
+      <Footer />
     </div>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-900 text-white py-6 text-center">
+      <div className="container mx-auto">
+        <h3 className="text-2xl font-bold mb-2">Contact Us</h3>
+        <p className="text-lg mb-2">123 Main St, Anytown, USA</p>
+        <p className="text-lg mb-2">(123) 456-7890</p>
+        (123) 456-7890
+        <p className="text-lg "><a href="mailto:example@example.com" className="text-white hover:text-gray-300">example@example.com</a></p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
